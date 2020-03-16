290a79584a63b6a8c1daa1e0d1f6bb2e415f9964
Hanna411 <hanzh@sheridancollege.ca>
2020-03-16 14:07:15 -0400

    update card class

--- src/project/Card.java
+++ src/project/Card.java
@@ -1,22 +1,16 @@
 /**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as a modifier and the date!
+ * SYST 17796 Project 
+ * Go Fish card game
+ * Group 2
+ * @Author Zhaoxu Han 
+ * 2020.03.16
  */
 package project;
 
 /**
- * A class to be used as the base Card class for the project. Must be general
- * enough to be instantiated for any Card game. Students wishing to add to the code 
- * should remember to add themselves as a modifier.
- * @author megha, 2020
+ * Card class
  */
 public enum Card {
     ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
 }
-    //default modifier for child classes
-    
-    /**
-     * Students should implement this method for their specific children classes 
-     * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
-     */
+
