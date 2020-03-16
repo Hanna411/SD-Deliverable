67d6696b4532a078648ebc6f83e942be2a977015
Hanna411 <hanzh@sheridancollege.ca>
2020-03-16 13:57:11 -0400

    use enum to set 13 cards

--- src/project/Card.java
+++ src/project/Card.java
@@ -11,16 +11,12 @@ package project;
  * should remember to add themselves as a modifier.
  * @author megha, 2020
  */
-public abstract class Card 
-{
+public enum Card {
+    ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING;
+}
     //default modifier for child classes
     
     /**
      * Students should implement this method for their specific children classes 
      * @return a String representation of a card. Could be an UNO card, a regular playing card etc.
      */
-    
-    @Override
-    public abstract String toString();
-    
-}
